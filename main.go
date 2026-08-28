b27daaebde5e68b4f417a26f77c8a2cd103bb5c7
jncogs <cogswell.jesse@gmail.com>
2026-08-28 16:00:19 -0500

    Added cd workflow

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -95,6 +96,6 @@ func main() {
 		ReadHeaderTimeout: 2 * time.Second,
 	}
 
-	//log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", strconv.Quote(port))
 	log.Fatal(srv.ListenAndServe())
 }
